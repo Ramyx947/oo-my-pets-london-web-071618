57131238464edf78c800b298862ed5d58dec9bb2
Ramona M <r1mari@yahoo.com>
2018-07-19 15:10:50 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -24,7 +24,24 @@ def self.count
   OWNERS.length
 end
 def say_species
-  puts "I am #{species}."
+  return "I am a #{species}."
+end 
+
+def buy_fish
+end
+def buy_cat
+end
+def buy_dog
+end
+def walk_dog
+end
+def play_with_cats
+end
+def feed_fish
+end
+def sell_pets
+end
+def list_pets
 end 
  
 end
